4bc20a2810adda1e7c51b3eb58f86030ae61ed47
Utkarsh Mandape <utmandape4@gmail.com>
2023-12-12 19:35:16 +0530

    Fix: bug

--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -53,11 +53,11 @@ const Landing = ({  match }) => {
       ) : (
         <>
           <Row>
-            {products.map((product) => (
+            {/* {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4}>
                 <Product product={product} />
               </Col>
-            ))}
+            ))} */}
           </Row>
           <Paginate
             className='paginate'
